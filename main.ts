c3050d998c2945c3083b9c4079dd90266f761839
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-03-26 13:08:30 +0800

    ...

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 //% color=0 weight=0 icon="\uf8cc" block="Input"
 namespace buttons.advanced {
+    //% block="on event $butt $presses"
+    export function onEvent(butt: string, presses: number, handler: () => void) {
+        // handler("Hello world", true);
+        button(butt, presses)
+    }
     //% block="when button $albab (A, B, A+B, logo) is pressed $number_of_times times in a row"
     export function button (albab: string, number_of_times: number) {
         if (albab == "A" || albab == "a") {
@@ -65,4 +70,68 @@ namespace buttons.advanced {
         }
         return true
     }   
-}   
+}  
+//function button (albab: string, number_of_times: number) {
+//    if (albab == "A" || albab == "a") {
+//        for (let index = 0; index < number_of_times; index++) {
+//            while (input.buttonIsPressed(Button.A)) {
+//                basic.pause(1)
+//            }
+//            for (let index = 0; index < 8; index++) {
+//                basic.pause(25)
+//                if (input.buttonIsPressed(Button.A)) {
+//                    break;
+//                }
+//            }
+//            if (!(input.buttonIsPressed(Button.A))) {
+//                return false
+//            }
+//        }
+//    } else if (albab == "B" || albab == "b") {
+//        for (let index = 0; index < number_of_times; index++) {
+//            while (input.buttonIsPressed(Button.B)) {
+//                basic.pause(1)
+//            }
+//            for (let index = 0; index < 8; index++) {
+//                basic.pause(25)
+//                if (input.buttonIsPressed(Button.B)) {
+//                    break;
+//                }
+//            }
+//            if (!(input.buttonIsPressed(Button.B))) {
+//                return false
+//            }
+//        }
+//    } else if (albab == "A+B" || (albab == "AB" || (albab == "a+B" || (albab == "aB" || (albab == "A+b" || (albab == "Ab" || (albab == "ab" || (albab == "a+b" || (albab == "B+A" || (albab == "BA" || (albab == "B+a" || (albab == "Ba" || (albab == "b+A" || (albab == "bA" || (albab == "ba" || albab == "b+a"))))))))))))))) {
+//        for (let index = 0; index < number_of_times; index++) {
+//            while (input.buttonIsPressed(Button.AB)) {
+//                basic.pause(1)
+//            }
+//            for (let index = 0; index < 8; index++) {
+//                basic.pause(25)
+//                if (input.buttonIsPressed(Button.AB)) {
+//                    break;
+//                }
+//            }
+//            if (!(input.buttonIsPressed(Button.AB))) {
+//                return false
+//            }
+//        }
+//    } else if (albab == "Logo" || (albab == "Logo" || albab == "LOGO")) {
+//        for (let index = 0; index < number_of_times; index++) {
+//            while (input.logoIsPressed()) {
+//                basic.pause(1)
+//            }
+//            for (let index = 0; index < 8; index++) {
+//                basic.pause(25)
+//                if (input.logoIsPressed()) {
+//                    break;
+//                }
+//            }
+//            if (!(input.logoIsPressed())) {
+//                return false
+//            }
+//        }
+//    }
+//    return true
+//}
