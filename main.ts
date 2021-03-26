17148387d6f45fc998e5566cba329d8024d78f0b
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-03-26 16:16:23 +0800

    buttons extension that is simpler

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
-//% color=0 weight=0 icon="\uf8cc" block="Input"
-namespace buttons.advanced {
-    //% block="on event $butt $presses"
-    export function onEvent(butt: string, presses: number, handler: () => void) {
-        // handler("Hello world", true);
-        button(butt, presses)
-    }
-    //% block="when button $albab (A, B, A+B, logo) is pressed $number_of_times times in a row"
-    export function button (albab: string, number_of_times: number) {
-        if (albab == "A" || albab == "a") {
+enum Buttons {
+//% block="Button A"
+A,
+
+//% block="Button B"
+B,
+
+//% block="Button A+B"
+AB,
+
+//% block="Logo"
+Logo,
+}
+//% color=0 weight=0 icon="\uf2dc" block="Input"
+namespace oof {
+    //% blockId=oof
+    //% block="when button $albab is pressed $number_of_times times in a row"  export function button (albab: string, number_of_times: number) {
+    export function button (albab: Buttons, number_of_times: number) {
+        if (albab = 1) {
             for (let index = 0; index < number_of_times; index++) {
                 while (input.buttonIsPressed(Button.A)) {
                     basic.pause(1)
@@ -22,7 +31,7 @@ namespace buttons.advanced {
                     return false
                 }
             }
-        } else if (albab == "B" || albab == "b") {
+        } else if (albab = 2) {
             for (let index = 0; index < number_of_times; index++) {
                 while (input.buttonIsPressed(Button.B)) {
                     basic.pause(1)
@@ -37,7 +46,7 @@ namespace buttons.advanced {
                     return false
                 }
             }
-        } else if (albab == "A+B" || (albab == "AB" || (albab == "a+B" || (albab == "aB" || (albab == "A+b" || (albab == "Ab" || (albab == "ab" || (albab == "a+b" || (albab == "B+A" || (albab == "BA" || (albab == "B+a" || (albab == "Ba" || (albab == "b+A" || (albab == "bA" || (albab == "ba" || albab == "b+a"))))))))))))))) {
+        } else if (albab = 3) {
             for (let index = 0; index < number_of_times; index++) {
                 while (input.buttonIsPressed(Button.AB)) {
                     basic.pause(1)
@@ -52,7 +61,7 @@ namespace buttons.advanced {
                     return false
                 }
             }
-        } else if (albab == "Logo" || (albab == "Logo" || albab == "LOGO")) {
+        } else if (albab = 4) {
             for (let index = 0; index < number_of_times; index++) {
                 while (input.logoIsPressed()) {
                     basic.pause(1)
