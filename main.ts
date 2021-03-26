18eb1fade8f7c5ebf514ab93a03790f66c2403ef
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-03-26 21:13:18 +0800

    changed the block id to something more unique, fixed a few problems, and shortened the code

--- main.ts
+++ main.ts
@@ -11,67 +11,43 @@ AB,
 //% block="Logo"
 Logo,
 }
-//% color=0 weight=0 icon="\uf2dc" block="Input"
-namespace oof {
-    //% blockId=oof
+//% color=#7AD7F0 weight=0 icon="\uf2dc" block="Input"
+namespace input2 {
+    //% blockId=button_press_35372836287323
     //% block="when button $albab is pressed $number_of_times times in a row"  export function button (albab: string, number_of_times: number) {
     export function button (albab: Buttons, number_of_times: number) {
         if (albab = 1) {
             for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.A)) {
-                    basic.pause(1)
-                }
-                for (let index = 0; index < 8; index++) {
-                    basic.pause(25)
-                    if (input.buttonIsPressed(Button.A)) {
-                        break;
-                    }
-                }
+                while (input.buttonIsPressed(Button.A)) {}
+                let start = control.millis()
+                while(!(input.buttonIsPressed(Button.A) || start + 200 <= control.millis())) {}
                 if (!(input.buttonIsPressed(Button.A))) {
                     return false
                 }
             }
         } else if (albab = 2) {
             for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.B)) {
-                    basic.pause(1)
-                }
-                for (let index = 0; index < 8; index++) {
-                    basic.pause(25)
-                    if (input.buttonIsPressed(Button.B)) {
-                        break;
-                    }
-                }
+                while (input.buttonIsPressed(Button.B)) {}
+                let start = control.millis()
+                while(!(input.buttonIsPressed(Button.B) || start + 200 <= control.millis())) {}
                 if (!(input.buttonIsPressed(Button.B))) {
                     return false
                 }
             }
         } else if (albab = 3) {
             for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.AB)) {
-                    basic.pause(1)
-                }
-                for (let index = 0; index < 8; index++) {
-                    basic.pause(25)
-                    if (input.buttonIsPressed(Button.AB)) {
-                        break;
-                    }
-                }
+                while (input.buttonIsPressed(Button.AB)) {}
+                let start = control.millis()
+                while(!(input.buttonIsPressed(Button.AB) || start + 200 <= control.millis())) {}
                 if (!(input.buttonIsPressed(Button.AB))) {
                     return false
                 }
             }
         } else if (albab = 4) {
             for (let index = 0; index < number_of_times; index++) {
-                while (input.logoIsPressed()) {
-                    basic.pause(1)
-                }
-                for (let index = 0; index < 8; index++) {
-                    basic.pause(25)
-                    if (input.logoIsPressed()) {
-                        break;
-                    }
-                }
+                while (input.logoIsPressed()) {}
+                let start = control.millis()
+                while(!(input.logoIsPressed() || start + 200 <= control.millis())) {}
                 if (!(input.logoIsPressed())) {
                     return false
                 }
