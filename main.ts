f5427e48a02e370b6135ba5a4df91662e1109eb5
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-03-27 12:29:34 +0800

    added "button held" and shortened code by a lot

--- main.ts
+++ main.ts
@@ -13,48 +13,45 @@ Logo,
 }
 //% color=#7AD7F0 weight=0 icon="\uf2dc" block="Input"
 namespace input2 {
+    function button2 (button: number) {
+        if (button = 1) {
+            return input.buttonIsPressed(Button.A)
+        } else if (button = 2) {
+            return input.buttonIsPressed(Button.B)
+        } else if (button = 3) {
+            return input.buttonIsPressed(Button.AB)
+        } else if (button = 4) {
+            return input.logoIsPressed()
+        }
+        return false
+    }
     //% blockId=button_press_35372836287323
-    //% block="when button $albab is pressed $number_of_times times in a row"  export function button (albab: string, number_of_times: number) {
+    //% block="when button $albab is pressed $number_of_times times in a row"
     export function button (albab: Buttons, number_of_times: number) {
-        if (albab = 1) {
-            for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.A)) {}
-                let start = control.millis()
-                while(!(input.buttonIsPressed(Button.A) || start + 200 <= control.millis())) {}
-                if (!(input.buttonIsPressed(Button.A))) {
-                    return false
+        x:
+        for (let index = 0; index < number_of_times; index++) {
+            while (button2(albab)) {}
+            let start = control.millis()
+            while(start + 200 > control.millis()) {
+                if(button2(albab)) {
+                    continue x
                 }
             }
-        } else if (albab = 2) {
-            for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.B)) {}
-                let start = control.millis()
-                while(!(input.buttonIsPressed(Button.B) || start + 200 <= control.millis())) {}
-                if (!(input.buttonIsPressed(Button.B))) {
-                    return false
-                }
-            }
-        } else if (albab = 3) {
-            for (let index = 0; index < number_of_times; index++) {
-                while (input.buttonIsPressed(Button.AB)) {}
-                let start = control.millis()
-                while(!(input.buttonIsPressed(Button.AB) || start + 200 <= control.millis())) {}
-                if (!(input.buttonIsPressed(Button.AB))) {
-                    return false
-                }
-            }
-        } else if (albab = 4) {
-            for (let index = 0; index < number_of_times; index++) {
-                while (input.logoIsPressed()) {}
-                let start = control.millis()
-                while(!(input.logoIsPressed() || start + 200 <= control.millis())) {}
-                if (!(input.logoIsPressed())) {
-                    return false
-                }
+            return false
+        }
+        return true
+    }  
+    //% blockId=button_press_3828372938287328392793289372832
+    //% block="when button: $button is held for $time (ms)"
+    export function button3 (button: Buttons, time: number) {
+        let start = control.millis()
+        while(start + time > control.millis()) {
+            if(!(button2(button))) {
+                return false
             }
         }
         return true
-    }   
+    }
 }  
 //function button (albab: string, number_of_times: number) {
 //    if (albab == "A" || albab == "a") {
